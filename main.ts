f1f4809090ad18b32019dcd5e38ced30a6a70fac
yoyo0281 <40514257+yoyo0281@users.noreply.github.com>
2020-12-01 11:31:54 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-
+let n = ""
+let m = ""
+let list = [0, 1, 2, 3, 4, 5, 6, 7]
+let text_list = ["0", "1", "2", "3", "4", "5", "6", "7"]
+basic.forever(function () {
+    m = ""
+    m = ""
+    for (let index = 0; index <= text_list.length - 1; index++) {
+        if (m == "") {
+            m = text_list[index]
+            n = text_list[index + 1]
+            text_list[0] = text_list[text_list.length - 1]
+        } else {
+            text_list[index] = convertToText(m)
+            m = n
+            n = text_list[index + 1]
+        }
+    }
+    for (let index = 0; index <= text_list.length - 1; index++) {
+        basic.showString("" + (text_list[index]))
+        basic.pause(10)
+    }
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+})
